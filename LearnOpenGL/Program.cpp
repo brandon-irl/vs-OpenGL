0077cf8bb0c6c3ac69e1647fe27c22ceff8cd8e3
haxard <haxard10@hotmail.com>
2015-10-20 23:10:17 -0400

    Began Hello Triangle
    Up to "Vertex Shader" section

--- LearnOpenGL/Program.cpp
+++ LearnOpenGL/Program.cpp
@@ -12,7 +12,6 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-
 	// Create a GLFWwindow object that we can use for GLFW's functions
 	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
 	if (window == nullptr)
@@ -22,6 +21,8 @@ int main()
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
+	//Set esc to close callback
+	glfwSetKeyCallback(window, key_callback);
 	// Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
 	glewExperimental = GL_TRUE;
 	// Initialize GLEW to setup the OpenGL Function pointers
@@ -34,11 +35,27 @@ int main()
 	// Define the viewport dimensions
 	glViewport(0, 0, WIDTH, HEIGHT);
 
+	//TRIANGLE
+	GLfloat vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f,-0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f
+	};
+	GLuint VBO;
+	glGenBuffers(1, &VBO);	//Create buffer (which is VBO)
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);	//Bind VBO to the type of a vertex buffer object
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); //Put the vertex array into the buffers memory
+
 	// Game loop
 	while (!glfwWindowShouldClose(window))
 	{
 		// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
 		glfwPollEvents();
+
+		//Rendering commands
+		glClearColor(0.5f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
 		// Swap the screen buffers
 		glfwSwapBuffers(window);
 	}
@@ -46,4 +63,9 @@ int main()
 	// Terminate GLFW, clearing any resources allocated by GLFW.
 	glfwTerminate();
 	return 0;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
 }
